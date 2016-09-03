edc0254d8e3bbb9388748ddc4cbd9048516a1227
jonschlinkert <jon.schlinkert@sellside.com>
2016-09-03 14:05:00 -0400

    support for escaped characters

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 /*!
  * is-extglob <https://github.com/jonschlinkert/is-extglob>
  *
- * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Copyright (c) 2014-2016, Jon Schlinkert.
  * Licensed under the MIT License.
  */
 
 module.exports = function isExtglob(str) {
-  return typeof str === 'string'
-    && /[@?!+*]\(/.test(str);
+  if (!str || typeof str !== 'string') {
+    return false;
+  }
+
+  var m, matches = [];
+  while ((m = /(\\).|([@?!+*]\()/g.exec(str))) {
+    if (m[2]) matches.push(m[2]);
+    str = str.slice(m.index + m[0].length);
+  }
+  return matches.length;
 };
